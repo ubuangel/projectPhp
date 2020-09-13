88c8dcd7994ddeaa118807edc7e14251385f801f
angel <andresbjar97@gmail.com>
2020-09-13 10:11:31 -0500

    creando el formulario

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php  include 'inc/layout/header.php';?>
 <h1>collasdasdangel</h1>
+
+
+<div class="bg-amarillo  contenedo sombra">
+	<form id="contactao" action="#">
+		<legend>
+			añada un contacto <span>Todos los acpos son obligatorios</span>
+		</legend>
+		<div class="campos">
+		<div class="campo">
+		<label for="nombre"> </label>
+		<input type="text" placeholder="nombre contacto" id="nombre">
+		</div>
+			
+
+		</div>
+	</form>
+
+</div>
 <?php  include 'inc/layout/footer.php';?>
