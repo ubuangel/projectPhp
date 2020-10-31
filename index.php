85237ed80641f1400120a4100e26e78fa2932872
angel <andresbjar97@gmail.com>
2020-10-31 18:16:27 -0500

    agregando snipet boxsizing rpermite  padding defines 200 px estatecnica agregas un with el elemnto se mantenga

--- index.php
+++ index.php
@@ -4,31 +4,32 @@
 	<h1>Agenda de contactos</h1>
 </div>
 
-<div class="bg-amarillo  contenedor sombra">
-    <form  id="contacto" action="#" >
-    	<legend>Añada un contacto <span>los campos son obligatorios</span></legend>
-    	<div class="campos">
-    	
+<div class="bg-amarillo contenedor sombra">
+    <form action="#" id="contacto">
+    	<legend> Añadir un contacto <span> todos los campos son obligatorios</span></legend>
+    	<div class=" campos">
     		<div class="campo">
-    			<label for=" nombre">nombre </label>
+    			<label for="nombre" >nombre</label>
     			<input type="text" placeholder="nombre contacto" id="nombre">
     		</div>
     		
     		<div class="campo">
-    			<label for=" nombre">Empresa </label>
+    			<label for="nombre" >Empresa</label>
     			<input type="text" placeholder="nombre contacto" id="Empresa">
     		</div>
     		
     		<div class="campo">
-    			<label for=" nombre">telefono: </label>
+    			<label for="nombre" >telefono</label>
     			<input type="tel" placeholder="nombre contacto" id="nombre">
     		</div>
     		
     		<div class="campo enviar">
-    			<input type="submit" value="Añadir">
+    			<input type="submit" value="Anadir" >
     		</div>
-    	</div>
+    	
+    	 </div>
     </form>
+
 </div>
 
 <?php  include 'inc/layout/footer.php';?>
