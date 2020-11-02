96276685f5f9d567f3468860bbe28079697f72cc
angel <andresbjar97@gmail.com>
2020-11-01 23:52:57 -0500

    gestionando mejor  app js

--- inc/funciones/bd.php
+++ inc/funciones/bd.php
@@ -5,5 +5,8 @@ define('DB_USUARIO', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_HOST', 'localhost');
 define('DB_NOMBRE', 'agendaphp');
-
+//mysqli solo ..
+//pdo 12 y 13  tipos de base de datos
 $conn = new mysqli(DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NOMBRE );
+
+//echo $conn->ping(); bunea forma si esta siendo conexino 1 si 0 no
--- inc/modelos/modelo-contactos.php
+++ inc/modelos/modelo-contactos.php
@@ -2,25 +2,31 @@
 
 
 if ($_POST['action']=='crear') {
+    //creara un nuevo registro
     require_once( '../funciones/bd.php');//importar base de datos con bd
     //validar las entradas
     
     $nombre=filter_var($_POST['nombre'],FILTER_SANITIZE_STRING);//validar limpiar entradas
     $empresa=filter_var($_POST['nombre'],FILTER_SANITIZE_STRING);
     $telefono=filter_var($_POST['nombre'],FILTER_SANITIZE_STRING);
+    
     try {//programano deje de funcionar
         $state=$conn->prepare("insert into contactos(nombre,empresa,telefono ) VALUES(?,?,?)");
         $state->bind_param("sss","$nombre","$empresa","$telefono");//inserta en bd//utilisara prepare statement ,la inyeccion de sql queda descartada
         $state->execute();
         
+        $state->close();
+        $conn->close();//ayudamos asql a cerra
+        
         $respuesta=array(
-            'respuesta'=>'correcto',
-            'info'=>$state
+            'respuesta'=>'correcto',// me la manda por json y como ya la estoy  leyendo con xhrresponce text
+             'info'=>$state->affected_rows//afecter rose  es una forma de saber si hubo cambios  en la base de datos es una forma de saber
+             
             
+          
         );
         
-        $state->close();
-        $conn->close();//ayudamos asql a cerra 
+       
     } catch (Exception $e) {//erreglo lo vamoms a converir json
         $respuesta=array('error'=>$e->getMessage()//e va trae el mensaje 
             
