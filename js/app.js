4de7c6102e3e157c0ad183b7ba4f96bcbb6bac50
angel <andresbjar97@gmail.com>
2020-11-02 00:41:33 -0500

    agregando todo para que funcione sin conflicto

--- js/app.js
+++ js/app.js
@@ -1,13 +1,48 @@
-//lanotificacion es por css y la validacin esta echaṕr medio de java script
-
 const formularioContactos=document.querySelector('#contacto');//declaramos de forma global ,lo voy a llamr difrentes parte del codigo entonces ,para no estar  declarandola misma varaible
 
 eventListeners();
 
 function eventListeners(){
-//	formularioContactos.addEventListener('submit',leerformulario);//llamar emediatamentemente ejecute le codigo
-	console.log();
-	estaen larama1 ?
+	formularioContactos.addEventListener('submit',leerformulario);//llamar emediatamentemente ejecute le codigo
+}
+
+function leerformulario(e){
+	
+	e.preventDefault();
+	
+	/* lee datos de los inputs*/
+	const nombre=document.querySelector('#nombre').value,
+	empresa=document.querySelector('#empresa').value,
+	
+	telefono=document.querySelector('#telefono').value;
+//	accion=document.querySelector('#accion').value;/*  como le di id que se llama  accion tengo que crear esa variable para que sepa el mismo formulario va terner editar otro de crear nuevo */
+	
+	if (nombre==''||empresa==''||telefono=='') {
+		console.log('todos los campos estan vcios');
+	}else{
+		console.log('tiene algo');
+	}
+
 }
 
 
+
+//notificacion en pantalla
+function mostrarNotificacion(){
+	const notificacion=document.createElement('div');
+	notificacion,textContent='hubo un error';
+	
+	//formulario
+	
+	formularioContactos.insertBefore(notificacion,document.querySelector('form legend'));
+	
+	
+	//ocultar y nostra la notificacion
+	
+	setTimeout(() => {
+		notificacion,classList.add('visible');
+		setTimeout(() => {
+			notificacion,classList.remove('visible');
+		}, 3000);
+	}, 100);//sper una dcimaparte de un segundo
+}
